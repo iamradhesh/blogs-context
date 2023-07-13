8fbc36bfd1b97a9452950d159fdeef25d6abb04d
iamradhesh <radhesh185@gmail.com>
2023-07-14 00:53:20 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./components/Pagination";
+
+
 export default function App() {
-  return <div>Hello World</div>;
+
+      console.log("I am inside app");
+     
+      return(
+                <div>
+                    <Header/>
+                    <Blogs/>
+                    <Pagination/>
+                </div>
+      )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+
+  console.log("I am inside a header");
+  return (
+    <div>
+            <header>
+                <h1> My Blogs</h1>
+            </header>
+    </div>
+  )
+}
+
+export default Header
