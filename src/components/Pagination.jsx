ebeb65ab68ed5e7e544344988cd9838da21c1bb1
iamradhesh <radhesh185@gmail.com>
2023-07-16 01:11:50 +0530

    pagintion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
+ 
+  const {page,handlePageChange,totalPages}=useContext(AppContext);
+ 
+ 
   return (
     <div>
-      
+        <footer>
+          {
+            page>1 &&
+            <button onClick={()=>handlePageChange(page-1)}>
+                Previous 
+            </button>
+          }
+          {
+            page<totalPages &&
+            <button onClick={()=>handlePageChange(page+1)}>
+                Next
+            </button>
+          }
+          <p>
+             Page {page} of {totalPages}
+          </p>
+        </footer>
     </div>
   )
 }
